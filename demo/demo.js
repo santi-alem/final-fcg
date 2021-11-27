ea883a8e90a64505d7ea06a3fb9722692099884c
huaqui123 <huaqui1997@gmail.com>
2021-11-27 00:28:50 -0300

    agrego lo de sky a medias, la última línea de drawSky sacandola estamos todo piola

--- demo/demo.js
+++ demo/demo.js
@@ -12,6 +12,16 @@ let depthFramebuffer;
 let shadowProgramInfo;
 const depthTextureSize = 2160;
 
+let skyProgramInfo;
+let texture;
+//Estas tres arrancan al principio de drawSky
+let skyboxLocation;
+let positionLocation;
+let viewDirectionProjectionInverseLocation;
+
+let positionBuffer; //Buffer del box
+let faceInfos; //Caras del box
+
 // Todo: Sacar los settings que no hacen nada
 const settings = {
     lightX: 0.1,
@@ -40,6 +50,35 @@ function setUpWebGL() {
     // setup GLSL programs
     toonProgramInfo = webglUtils.createProgramInfo(gl, ['vertex-shader-toon', 'fragment-shader-toon']);
     shadowProgramInfo = webglUtils.createProgramInfo(gl, ['color-vertex-shader', 'color-fragment-shader']);
+    skyProgramInfo = webglUtils.createProgramInfo(gl, ['vertex-shader-sky', 'fragment-shader-sky']);
+
+    faceInfos = [
+        {
+            target: gl.TEXTURE_CUBE_MAP_POSITIVE_X,
+            url: 'https://webglfundamentals.org/webgl/resources/images/computer-history-museum/pos-x.jpg',
+        },
+        {
+            target: gl.TEXTURE_CUBE_MAP_NEGATIVE_X,
+            url: 'https://webglfundamentals.org/webgl/resources/images/computer-history-museum/neg-x.jpg',
+        },
+        {
+            target: gl.TEXTURE_CUBE_MAP_POSITIVE_Y,
+            url: 'https://webglfundamentals.org/webgl/resources/images/computer-history-museum/pos-y.jpg',
+        },
+        {
+            target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Y,
+            url: 'https://webglfundamentals.org/webgl/resources/images/computer-history-museum/neg-y.jpg',
+        },
+        {
+            target: gl.TEXTURE_CUBE_MAP_POSITIVE_Z,
+            url: 'https://webglfundamentals.org/webgl/resources/images/computer-history-museum/pos-z.jpg',
+        },
+        {
+            target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Z,
+            url: 'https://webglfundamentals.org/webgl/resources/images/computer-history-museum/neg-z.jpg',
+        },
+    ];
+
     // imageTexture = loadImageTexture('https://raw.githubusercontent.com/gfxfundamentals/webgl-fundamentals/master/webgl/resources/models/windmill/windmill_001_base_COL.jpg');
     set_depth_buffer();
     setSettingUI();
@@ -98,6 +137,145 @@ function drawScene() {
     gl.disable(gl.CULL_FACE) // Eliminamos Las caras que enfrentan a la camara/luz
     // Rendereamos la escena de nuevo para el con el toon shader
     drawModels(lightWorldMatrix, lightProjectionMatrix, mv, mvp, perspectiveMatrix);
+
+    //render sky
+    drawSky(mvp);
+}
+
+function drawSky(mvp) {
+
+    let prog = skyProgramInfo.program;
+
+    // look up where the vertex data needs to go.
+    positionLocation = gl.getAttribLocation(prog, "a_position");
+
+    // lookup uniforms
+    skyboxLocation = gl.getUniformLocation(prog, "u_skybox");
+    viewDirectionProjectionInverseLocation =
+        gl.getUniformLocation(prog, "u_viewDirectionProjectionInverse");
+
+    positionBuffer = gl.createBuffer();
+    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // Put the positions in the buffer
+    setGeometry(gl);
+    
+    // Create a texture.
+    texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+
+    faceInfos.forEach((faceInfo) => {
+        const { target, url } = faceInfo;
+
+        // Upload the canvas to the cubemap face.
+        const level = 0;
+        const internalFormat = gl.RGBA;
+        const width = 512;
+        const height = 512;
+        const format = gl.RGBA;
+        const type = gl.UNSIGNED_BYTE;
+
+        // setup each face so it's immediately renderable
+        gl.texImage2D(target, level, internalFormat, width, height, 0, format, type, null);
+
+        // Asynchronously load an image
+        const image = new Image();
+        image.src = url;
+        image.crossOrigin = "anonymous";
+        image.addEventListener('load', function () {
+            // Now that the image has loaded make copy it to the texture.
+            gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+            gl.texImage2D(target, level, internalFormat, format, type, image);
+            gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
+        });
+    });
+    gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+
+    let fieldOfViewRadians = degToRad(60);
+    let cameraYRotationRadians = degToRad(0);
+
+    let time = 0.5;
+
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    gl.enable(gl.CULL_FACE);
+    gl.enable(gl.DEPTH_TEST);
+
+    // Clear the canvas AND the depth buffer.
+    //gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.useProgram(prog);
+
+    // Turn on the position attribute
+    gl.enableVertexAttribArray(positionLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        positionLocation, size, type, normalize, stride, offset);
+
+    // Compute the projection matrix
+    var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    var projectionMatrix =
+        m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
+
+    // camera going in circle 2 units from origin looking at origin
+    var cameraPosition = [Math.cos(time * .1), 0, Math.sin(time * .1)];
+    var target = [0, 0, 0];
+    var up = [0, 1, 0];
+    // Compute the camera's matrix using look at.
+    var cameraMatrix = m4.lookAt(cameraPosition, target, up);
+
+    // Make a view matrix from the camera matrix.
+    var viewMatrix = m4.inverse(cameraMatrix);
+
+    // We only care about direciton so remove the translation
+    viewMatrix[12] = 0;
+    viewMatrix[13] = 0;
+    viewMatrix[14] = 0;
+
+    var viewDirectionProjectionMatrix =
+        m4.multiply(projectionMatrix, viewMatrix);
+    var viewDirectionProjectionInverseMatrix =
+        m4.inverse(viewDirectionProjectionMatrix);
+
+    // Set the uniforms
+    gl.uniformMatrix4fv(
+        viewDirectionProjectionInverseLocation, false,
+        viewDirectionProjectionInverseMatrix);
+
+    // Tell the shader to use texture unit 0 for u_skybox
+    gl.uniform1i(skyboxLocation, 0);
+
+    // let our quad pass the depth test at 1.0
+    gl.depthFunc(gl.LEQUAL);
+
+    // Draw the geometry.
+    gl.drawArrays(gl.TRIANGLES, 0, 1 * 6);
+}
+
+// Fill the buffer with the values that define a quad.
+//FUNCIÓN PARA EL SKY
+function setGeometry(gl) {
+    var positions = new Float32Array(
+        [
+            -1, -1,
+            1, -1,
+            -1, 1,
+            -1, 1,
+            1, -1,
+            1, 1,
+        ]);
+    gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
 }
 
 function drawShadows(lightWorldMatrix, lightProjectionMatrix) {
