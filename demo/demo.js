d35ae716b307f9fdb2366aa578125bd6b3752b30
Santiago Alem <santialem.trev@gmail.com>
2022-02-09 18:54:40 -0300

    - Ahora renderea cuando se terminan de cargar las texturas

--- demo/demo.js
+++ demo/demo.js
@@ -260,6 +260,7 @@ function setSkyBoxTexture() {
             gl.bindTexture(gl.TEXTURE_CUBE_MAP, skyTexture);
             gl.texImage2D(target, level, internalFormat, format, type, image);
             gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
+            render();// Rendereamos cuando carga la textura;
         });
     });
     gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
@@ -472,6 +473,7 @@ function loadImageTexture(url) {
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
         gl.generateMipmap(gl.TEXTURE_2D);
+        render();// Rendereamos cuando carga la textura;
     });
     return texture;
 }
